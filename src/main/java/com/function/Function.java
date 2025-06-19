24ca1f5821bac63d1c1ca0c023925f6279d19642
Kisara de Silva <manmithdesilva@gmail.com>
2025-06-19 13:31:28 +0530

    Modify the Function to Return JSON using Query Parameters
    Objective:
    Update the function to accept a name via query parameter and return a JSON response.
    
    Input:
    GET request to: https://testaiaintern.azurewebsites.net/api/HttpExample?
    
    Expected Output:
    json { "message": "Hello, John" }

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -1,20 +1,26 @@
 package com.function;
 
 import java.util.Optional;
-
-import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.google.gson.Gson;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.*;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
+
+    private static class JsonResponse {
+        private final String message;
+
+        public JsonResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
     /**
      * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
@@ -28,16 +34,25 @@ public class Function {
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
+
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        final String name = request.getQueryParameters().get("name");
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        if (name == null || name.isEmpty()) {
+            JsonResponse response = new JsonResponse("Please provide a name query parameter");
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .header("Content-Type", "application/json")
+                .body(new Gson().toJson(response))
+                .build();
         }
+
+        JsonResponse response = new JsonResponse("Hello, " + name);
+        return request.createResponseBuilder(HttpStatus.OK)
+                .header("Content-Type", "application/json")
+                .body(new Gson().toJson(response))
+                .build();
+        
     }
 }
